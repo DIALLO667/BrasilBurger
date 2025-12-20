5a94ec4fc008d448aca02f80b03d53847348ae2e
Macbook Pro 2020 <macbookpro2020@192.168.1.2>
2025-12-20 14:22:42 +0000

    fix: Upgrade to .NET 10 + Port configuration fixes

--- csharp-client/BrasilBurger.Client/obj/Debug/net10.0/BrasilBurger.Client.AssemblyInfo.cs
+++ csharp-client/BrasilBurger.Client/obj/Debug/net10.0/BrasilBurger.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrasilBurger.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9235e79ced476736d53f2c852b5c9faa423318bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06ce3008946cd04abf699ed41037801643aaf2be")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrasilBurger.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrasilBurger.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
